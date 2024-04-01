47f14862f572556ba62ca715db1c301b1a02aeef
Keith Scheldt <keithscheldt@gmail.com>
2024-04-01 18:49:54 -0400

    Update global CSS and page title

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -46,7 +46,7 @@ const Menu = () => {
 
                 {/* sun icon */}
                 <svg
-                  className="swap-on fill-current w-10 h-10"
+                  className="swap-on fill-current w-6 h-6 mt-3 mr-2"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
@@ -55,7 +55,7 @@ const Menu = () => {
 
                 {/* moon icon */}
                 <svg
-                  className="swap-off fill-current w-10 h-10"
+                  className="swap-off fill-current w-6 h-6 mt-3 mr-2"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
